33be65f914094c81050c5669b9cdb1ca6c2881bd
Leon <LAnneken09@gmail.com>
2026-07-13 13:23:43 +0200

    Added the getMMRByPuuid endpoint for v1

---
+++ src/mmr/v1/getMMRByPuuid.ts
@@ -0,0 +1,30 @@
+import { doApiRequest } from "../../lib/request-handler";
+
+export async function v1_getMMRByPuuid(
+  apiKey: string,
+  affinity: string,
+  puuid: string,
+) {
+  return await doApiRequest<v1_getMMRByPuuid_ResponseData>(
+    apiKey,
+    `/v1/mmr/${affinity}/${puuid}`,
+    {},
+    { method: "GET" },
+  );
+}
+export interface v1_getMMRByPuuid_ResponseData {
+  currenttier: number;
+  currenttierpatched: string;
+  elo: number;
+  images: {
+    large: string;
+    small: string;
+    triangle_down: string;
+    triangle_up: string;
+  };
+  mmr_change_to_last_game: number;
+  name: string;
+  old: boolean;
+  ranking_in_tier: number;
+  tag: string;
+}
--- src/mmr/v1/index.ts
+++ src/mmr/v1/index.ts
@@ -1,4 +1,5 @@
 import { v1_getMMRByName } from "./getMMRByName";
+import { v1_getMMRByPuuid } from "./getMMRByPuuid";
 
 export class v1_MMRCategory {
   constructor(readonly apiKey: string) {}
@@ -12,5 +13,15 @@ export class v1_MMRCategory {
    */
   async getByName(affinity: string, name: string, tag: string) {
     return await v1_getMMRByName(this.apiKey, affinity, name, tag);
+  }
+
+  /**
+   * @link https://docs.henrikdev.xyz/api-reference/valorant/get-mmr-by-puuid-v1
+   * @param affinity Region/affinity (e.g., na, eu, ap, kr)
+   * @param puuid Player UUID
+   * @returns MMR data retrieved successfully
+   */
+  async getByPuuid(affinity: string, puuid: string) {
+    return await v1_getMMRByPuuid(this.apiKey, affinity, puuid);
   }
 }
