78ec1b6bf034f184509a7713ef2117f839297c65
Leon <LAnneken09@gmail.com>
2026-07-13 17:23:06 +0200

    Added the getVlrEsportsEvents endpoint to v2

---
+++ src/vlr/v2/getVlrEsportsEvents.ts
@@ -0,0 +1,39 @@
+import { doApiRequest } from "../../lib/request-handler";
+
+export async function v2_getVlrEsportsEvents(
+  apiKey: string,
+  region?:
+    | "north_america"
+    | "europe"
+    | "brazil"
+    | "asia_pacific"
+    | "korea"
+    | "japan"
+    | "latin_america"
+    | "oceania"
+    | "mena"
+    | "gc"
+    | "collegiate",
+  type?: "completed" | "upcoming",
+  page?: number,
+) {
+  return await doApiRequest<v2_getVlrEsportsEvents_ResponseData[]>(
+    apiKey,
+    "/v2/esports/vlr/events",
+    { region, type, page: page?.toString() },
+    { method: "GET" },
+  );
+}
+
+export interface v2_getVlrEsportsEvents_ResponseData {
+  dates: {
+    end: string;
+    start: string;
+  };
+  icon: string;
+  id: number;
+  price: string;
+  region: string;
+  slug: string;
+  title: string;
+}
--- src/vlr/v2/index.ts
+++ src/vlr/v2/index.ts
@@ -1,3 +1,31 @@
+import { v2_getVlrEsportsEvents } from "./getVlrEsportsEvents";
+
 export class v2_VlrCategory {
   constructor(readonly apiKey: string) {}
+
+  /**
+   * @link https://docs.henrikdev.xyz/api-reference/valorant/get-vlr-esports-events-v2
+   * @param region
+   * @param type
+   * @param page
+   * @returns Esports events retrieved successfully
+   */
+  async getEsportsEvents(
+    region?:
+      | "north_america"
+      | "europe"
+      | "brazil"
+      | "asia_pacific"
+      | "korea"
+      | "japan"
+      | "latin_america"
+      | "oceania"
+      | "mena"
+      | "gc"
+      | "collegiate",
+    type?: "completed" | "upcoming",
+    page?: number,
+  ) {
+    return await v2_getVlrEsportsEvents(this.apiKey, region, type, page);
+  }
 }
