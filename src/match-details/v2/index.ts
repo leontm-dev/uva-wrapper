8769a76d218e5df80133ec5f601c5bf71ff9e4b9
Leon <LAnneken09@gmail.com>
2026-07-13 15:54:32 +0200

    Added getMatchDetails endpoint for v2

--- src/match-details/v2/index.ts
+++ src/match-details/v2/index.ts
@@ -1,3 +1,14 @@
+import { v2_getMatchDetails } from "./getMatchDetails";
+
 export class v2_MatchDetailsCategory {
   constructor(readonly apiKey: string) {}
+
+  /**
+   * @link https://docs.henrikdev.xyz/api-reference/valorant/get-match-details-v2
+   * @param matchId Match UUID
+   * @returns Match details retrieved successfully
+   */
+  async get(matchId: string) {
+    return await v2_getMatchDetails(this.apiKey, matchId);
+  }
 }
