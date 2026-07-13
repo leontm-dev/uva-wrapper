19aef3d538cecbc8c3d538cdbd84bb2b790bca9c
Leon <LAnneken09@gmail.com>
2026-07-13 13:55:25 +0200

    Added the getMMRHistoryByName endpoint for v1

---
+++ src/mmr-history/v1/getMMRHistoryByName.ts
@@ -0,0 +1,42 @@
+import { doApiRequest } from "../../lib/request-handler";
+import {
+  DefaultApiResponse,
+  DefaultErrorsResponse,
+} from "../../types/response.type";
+
+export async function v1_getMMRHistoryByName(
+  apiKey: string,
+  affinity: string,
+  name: string,
+  tag: string,
+) {
+  return (await doApiRequest(
+    apiKey,
+    `/v1/mmr-history/${affinity}/${name}/${tag}`,
+    {},
+    { method: "GET" },
+  )) as
+    | (DefaultApiResponse<v1_getMMRHistoryByName_ResponseData> & {
+        name: string;
+        tag: string;
+      })
+    | DefaultErrorsResponse;
+}
+export interface v1_getMMRHistoryByName_ResponseData {
+  currenttier: number;
+  currenttierpatched: string;
+  date: string;
+  date_raw: number;
+  elo: number;
+  images: {
+    large: string;
+    small: string;
+    triangle_down: string;
+    triangle_up: string;
+  };
+  map: { id: string; name: string };
+  match_id: string;
+  mmr_change_to_last_game: number;
+  ranking_in_tier: number;
+  season_id: string;
+}
--- src/mmr-history/v1/index.ts
+++ src/mmr-history/v1/index.ts
@@ -1,3 +1,16 @@
+import { v1_getMMRHistoryByName } from "./getMMRHistoryByName";
+
 export class v1_MMRHistoryCategory {
   constructor(readonly apiKey: string) {}
+
+  /**
+   * @link https://docs.henrikdev.xyz/api-reference/valorant/get-mmr-history-by-name-v1
+   * @param affinity Region/affinity (e.g., na, eu, ap, kr)
+   * @param name Riot ID name
+   * @param tag Riot ID tag
+   * @returns MMR history retrieved successfully
+   */
+  async getByName(affinity: string, name: string, tag: string) {
+    return await v1_getMMRHistoryByName(this.apiKey, affinity, name, tag);
+  }
 }
