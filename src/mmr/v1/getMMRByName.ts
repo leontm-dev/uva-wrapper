c426c8f23b4ace7a4dd617fdd70f7431e8a14203
Leon <LAnneken09@gmail.com>
2026-07-13 13:21:34 +0200

    Added the getMMRByName endpoint for v1

---
+++ src/mmr/v1/getMMRByName.ts
@@ -0,0 +1,31 @@
+import { doApiRequest } from "../../lib/request-handler";
+
+export async function v1_getMMRByName(
+  apiKey: string,
+  affinity: string,
+  name: string,
+  tag: string,
+) {
+  return await doApiRequest<v1_getMMRByName_ResponseData>(
+    apiKey,
+    `/v1/mmr/${affinity}/${name}/${tag}`,
+    {},
+    { method: "GET" },
+  );
+}
+export interface v1_getMMRByName_ResponseData {
+  currenttier: number;
+  currenttierpatched: string;
+  elo: number;
+  images: {
+    large: string;
+    small: string;
+    triangle_down: string;
+    triangle_up: string;
+  };
+  mmr_change_to_last_game: number;
+  name: string;
+  old: boolean;
+  ranking_in_tier: number;
+  tag: string;
+}
--- src/mmr/v1/index.ts
+++ src/mmr/v1/index.ts
@@ -1,3 +1,16 @@
+import { v1_getMMRByName } from "./getMMRByName";
+
 export class v1_MMRCategory {
   constructor(readonly apiKey: string) {}
+
+  /**
+   * @link https://docs.henrikdev.xyz/api-reference/valorant/get-mmr-by-name-v1
+   * @param affinity Region/affinity (e.g., na, eu, ap, kr)
+   * @param name Riot ID name
+   * @param tag Riot ID tag
+   * @returns MMR data retrieved successfully
+   */
+  async getByName(affinity: string, name: string, tag: string) {
+    return await v1_getMMRByName(this.apiKey, affinity, name, tag);
+  }
 }
