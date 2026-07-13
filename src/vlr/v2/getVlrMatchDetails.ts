8ca2bcb1b27474f41598b5213e89048290e8080d
Leon <LAnneken09@gmail.com>
2026-07-13 17:34:55 +0200

    Added the getVlrMatchDetails endpoint to v2

---
+++ src/vlr/v2/getVlrMatchDetails.ts
@@ -0,0 +1,167 @@
+import { doApiRequest } from "../../lib/request-handler";
+
+export async function v2_getVlrMatchDetails(apiKey: string, matchId: number) {
+  return await doApiRequest<v2_getVlrMatchDetails_ResponseData>(
+    apiKey,
+    `/v2/esports/vlr/matches/${matchId}`,
+    {},
+    { method: "GET" },
+  );
+}
+
+export interface v2_getVlrMatchDetails_ResponseData {
+  games: {
+    map: string;
+    rounds: {
+      round: number;
+      winning_site: string;
+      winning_team: number;
+    }[];
+    teams: {
+      is_winner: boolean;
+      name: string;
+      players: {
+        agent: string;
+        player: {
+          id: number;
+          name: string;
+          nation: string;
+          slug: string;
+        };
+        stats: {
+          acs: number | null;
+          adr: number | null;
+          assists: number | null;
+          deaths: number | null;
+          first_deaths: number | null;
+          first_kills: number | null;
+          fk_diff: number | null;
+          hs_pct: number | null;
+          kast: number | null;
+          kd_diff: number | null;
+          kills: number | null;
+          rating: number | null;
+        };
+      }[];
+      score: number | null;
+      score_ct: number | null;
+      score_t: number | null;
+    }[];
+    duration_in_s: number | null;
+    picked_by: number | null;
+  }[];
+  head_to_head: {
+    date: string;
+    event: {
+      icon: string;
+      name: string;
+      series: string;
+    };
+    match: {
+      id: number;
+      slug: string;
+    };
+    score: {
+      team_1: number;
+      team_2: number;
+      winner_team: number;
+    };
+  }[];
+  metadata: {
+    date: string;
+    event: {
+      icon: string;
+      id: number;
+      series: string;
+      slug: string;
+      title: string;
+    };
+    format: string;
+    note: string;
+    patch: string;
+    status: string;
+  };
+  past_matches: {
+    matches: {
+      date: string;
+      is_win: boolean;
+      match: {
+        id: number;
+        slug: string;
+      };
+      opponent: {
+        logo: string;
+        name: string;
+      };
+      score: {
+        against: number;
+        for: number;
+      };
+    }[];
+    team: number;
+  }[];
+  streams: {
+    link: string;
+    name: string;
+  }[];
+  teams: {
+    icon: string;
+    id: number;
+    name: string;
+    slug: string;
+    url: string;
+    score: number | null;
+  }[];
+  vods: {
+    link: string;
+    name: string;
+  }[];
+  economy: {
+    teams: {
+      eco: {
+        rounds: number;
+        won: number;
+      };
+      full_buy: {
+        rounds: number;
+        won: number;
+      };
+      pistol_won: number;
+      semi_eco: {
+        rounds: number;
+        won: number;
+      };
+      team_name: string;
+    }[];
+  } | null;
+  performance: {
+    kill_matrix: {
+      deaths: number;
+      killer: number;
+      kills: number;
+      victim: number;
+    }[];
+    player_performances: {
+      clutches: {
+        "1v1": number;
+        "1v2": number;
+        "1v3": number;
+        "1v4": number;
+        "1v5": number;
+      };
+      defuses: number;
+      econ_rating: number;
+      multi_kills: {
+        "2k": number;
+        "3k": number;
+        "4k": number;
+        "5k": number;
+      };
+      plants: number;
+      player: {
+        id: number;
+        name: string;
+      };
+    }[];
+  } | null;
+}
--- src/vlr/v2/index.ts
+++ src/vlr/v2/index.ts
@@ -1,5 +1,6 @@
 import { v2_getVlrEsportsEvents } from "./getVlrEsportsEvents";
 import { v2_getVlrEventMatches } from "./getVlrEventMatches";
+import { v2_getVlrMatchDetails } from "./getVlrMatchDetails";
 
 export class v2_VlrCategory {
   constructor(readonly apiKey: string) {}
@@ -37,5 +38,14 @@ export class v2_VlrCategory {
    */
   async getEventMatches(eventId: number) {
     return await v2_getVlrEventMatches(this.apiKey, eventId);
+  }
+
+  /**
+   * @link https://docs.henrikdev.xyz/api-reference/valorant/get-vlr-match-details-v2
+   * @param matchId
+   * @returns Esports match details retrieved successfully
+   */
+  async getMatchDetails(matchId: number) {
+    return await v2_getVlrMatchDetails(this.apiKey, matchId);
   }
 }
