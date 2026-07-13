56fc174c96ac2a15be6a43186e0d85a3bf3cb885
Leon <LAnneken09@gmail.com>
2026-07-13 16:12:17 +0200

    Fixed mistake here :(

--- src/leaderboard/index.ts
+++ src/leaderboard/index.ts
@@ -4,9 +4,9 @@ import { v3_LeaderboardCategory } from "./v3";
 
 export class LeaderboardCategory {
   constructor(readonly apiKey: string) {
-    this.v1 = new LeaderboardCategory(this.apiKey);
-    this.v2 = new LeaderboardCategory(this.apiKey);
-    this.v3 = new LeaderboardCategory(this.apiKey);
+    this.v1 = new v1_LeaderboardCategory(this.apiKey);
+    this.v2 = new v2_LeaderboardCategory(this.apiKey);
+    this.v3 = new v3_LeaderboardCategory(this.apiKey);
   }
 
   v1: v1_LeaderboardCategory;
