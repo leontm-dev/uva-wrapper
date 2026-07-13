c237580906372c289ddc71a14d9a278358b85b3d
Leon <LAnneken09@gmail.com>
2026-07-13 16:32:50 +0200

    Fixed the v1 declaration by using the correct premier version class

--- src/premier/index.ts
+++ src/premier/index.ts
@@ -2,7 +2,7 @@ import { v1_PremierCategory } from "./v1";
 
 export class PremierCategory {
   constructor(readonly apiKey: string) {
-    this.v1 = new PremierCategory(this.apiKey);
+    this.v1 = new v1_PremierCategory(this.apiKey);
   }
 
   v1: v1_PremierCategory;
