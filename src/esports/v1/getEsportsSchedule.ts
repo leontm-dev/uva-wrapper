ebcb99a005b2d3b44df9b954f408fe80ba38fda1
Leon <LAnneken09@gmail.com>
2026-07-13 17:17:14 +0200

    Added the getEsportsSchedule endpoint

---
+++ src/esports/v1/getEsportsSchedule.ts
@@ -0,0 +1,49 @@
+import { doApiRequest } from "../../lib/request-handler";
+
+export async function v1_getEsportsSchedule(
+  apiKey: string,
+  region?: string,
+  league?: string,
+) {
+  return await doApiRequest<v1_getEsportsSchedule_ResponseData[]>(
+    apiKey,
+    "/v1/esports/schedule",
+    { region, league },
+    { method: "GET" },
+  );
+}
+
+export interface v1_getEsportsSchedule_ResponseData {
+  date: string;
+  league: {
+    icon: string;
+    identifier: string;
+    name: string;
+    region: string;
+  };
+  match: {
+    game_type: {
+      count: number;
+      type: string;
+    };
+    teams: {
+      code: string;
+      game_wins: number;
+      has_won: boolean;
+      icon: string;
+      name: string;
+      record: {
+        losses: number;
+        wins: number;
+      };
+    }[];
+    id: string;
+  };
+  state: string;
+  tournament: {
+    name: string;
+    season: string;
+  };
+  type: string;
+  vod: string;
+}
--- src/esports/v1/index.ts
+++ src/esports/v1/index.ts
@@ -1,3 +1,15 @@
+import { v1_getEsportsSchedule } from "./getEsportsSchedule";
+
 export class v1_EsportsCategory {
   constructor(readonly apiKey: string) {}
+
+  /**
+   * @link https://docs.henrikdev.xyz/api-reference/valorant/get-esports-schedule-v1
+   * @param region
+   * @param league
+   * @returns Esports schedule retrieved successfully
+   */
+  async getSchedule(region?: string, league?: string) {
+    return await v1_getEsportsSchedule(this.apiKey, region, league);
+  }
 }
