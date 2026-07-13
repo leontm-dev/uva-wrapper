5eb63b75ab6670c10cd2edea16711296d58947da
Leon <LAnneken09@gmail.com>
2026-07-13 17:06:31 +0200

    Added the getPremierTeamHistoryByName endpoint for v1

---
+++ src/premier-history/v1/getPremierTeamHistoryByName.ts
@@ -0,0 +1,32 @@
+import { doApiRequest } from "../../lib/request-handler";
+
+export async function v1_getPremierTeamHistoryByName(
+  apiKey: string,
+  name: string,
+  tag: string,
+  season?: string,
+) {
+  return await doApiRequest<v1_getPremierTeamHistoryByName_ResponseData>(
+    apiKey,
+    `/v1/premier/${name}/${tag}/history`,
+    { season },
+    { method: "GET" },
+  );
+}
+
+export interface v1_getPremierTeamHistoryByName_ResponseData {
+  league_matches: {
+    id: string;
+    points_after: number;
+    points_before: number;
+    started_at: string;
+  }[];
+  tournament_matches: {
+    matches: string[];
+    placement: number;
+    placement_league_bonus: number;
+    points_after: number;
+    points_before: number;
+    tournament_id: string;
+  }[];
+}
--- src/premier-history/v1/index.ts
+++ src/premier-history/v1/index.ts
@@ -1,3 +1,16 @@
+import { v1_getPremierTeamHistoryByName } from "./getPremierTeamHistoryByName";
+
 export class v1_PremierHistoryCategory {
   constructor(readonly apiKey: string) {}
+
+  /**
+   * @link https://docs.henrikdev.xyz/api-reference/valorant/get-premier-team-history-by-name-v1
+   * @param name Team name
+   * @param tag Team tag
+   * @param season Premier season id (optional)
+   * @returns Premier team history retrieved successfully
+   */
+  async getByName(name: string, tag: string, season?: string) {
+    return await v1_getPremierTeamHistoryByName(this.apiKey, name, tag, season);
+  }
 }
