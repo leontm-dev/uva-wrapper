0560d20d08a8eb63315848917bcf224d849958fe
Leon <LAnneken09@gmail.com>
2026-07-13 22:16:40 +0200

    Added all available versions to the premier category

--- src/premium/index.ts
+++ src/premium/index.ts
@@ -1,3 +1,9 @@
+import { v1_PremiumCategory } from "./v1";
+
 export class PremiumCategory {
-  constructor(readonly apiKey: string) {}
+  constructor(readonly apiKey: string) {
+    this.v1 = new v1_PremiumCategory(this.apiKey);
+  }
+
+  v1: v1_PremiumCategory;
 }
