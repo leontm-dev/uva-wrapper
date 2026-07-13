9e7b8d45b996bb327c8ef7cebb59cba4084ee349
Leon <LAnneken09@gmail.com>
2026-07-13 13:58:09 +0200

    Added the getMMRHistoryByPuuid endpoint to v1

---
+++ src/mmr-history/v1/getMMRHistoryByPuuid.ts
@@ -0,0 +1,41 @@
+import { doApiRequest } from "../../lib/request-handler";
+import {
+  DefaultApiResponse,
+  DefaultErrorsResponse,
+} from "../../types/response.type";
+
+export async function v1_getMMRHistoryByPuuid(
+  apiKey: string,
+  affinity: string,
+  puuid: string,
+) {
+  return (await doApiRequest(
+    apiKey,
+    `/v1/by-puuid/mmr-history/${affinity}/${puuid}`,
+    {},
+    { method: "GET" },
+  )) as
+    | (DefaultApiResponse<v1_getMMRHistoryByPuuid_ResponseData> & {
+        name: string;
+        tag: string;
+      })
+    | DefaultErrorsResponse;
+}
+export interface v1_getMMRHistoryByPuuid_ResponseData {
+  currenttier: number;
+  currenttierpatched: string;
+  date: string;
+  date_raw: number;
+  elo: number;
+  images: {
+    large: string;
+    small: string;
+    triangle_down: string;
+    triangle_up: string;
+  };
+  map: { id: string; name: string };
+  match_id: string;
+  mmr_change_to_last_game: number;
+  ranking_in_tier: number;
+  season_id: string;
+}
--- src/mmr-history/v1/index.ts
+++ src/mmr-history/v1/index.ts
@@ -1,4 +1,5 @@
 import { v1_getMMRHistoryByName } from "./getMMRHistoryByName";
+import { v1_getMMRHistoryByPuuid } from "./getMMRHistoryByPuuid";
 
 export class v1_MMRHistoryCategory {
   constructor(readonly apiKey: string) {}
@@ -12,5 +13,15 @@ export class v1_MMRHistoryCategory {
    */
   async getByName(affinity: string, name: string, tag: string) {
     return await v1_getMMRHistoryByName(this.apiKey, affinity, name, tag);
+  }
+
+  /**
+   * @link https://docs.henrikdev.xyz/api-reference/valorant/get-mmr-history-by-puuid-v1
+   * @param affinity Region/affinity (e.g., na, eu, ap, kr)
+   * @param puuid Player UUID
+   * @returns MMR history retrieved successfully
+   */
+  async getByPuuid(affinity: string, puuid: string) {
+    return await v1_getMMRHistoryByPuuid(this.apiKey, affinity, puuid);
   }
 }
