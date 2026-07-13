9419d2919411270cce8dc4502215ca11f86b14eb
Leon <LAnneken09@gmail.com>
2026-07-13 13:14:58 +0200

    Added the MMR category to the main collection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { AccountCategory } from "./account";
+import { MMRCategory } from "./mmr";
 
 export class UnofficialValorantAPI {
   constructor(readonly apiKey: string) {
     this.Account = new AccountCategory(this.apiKey);
+    this.MMR = new MMRCategory(this.apiKey);
   }
 
   Account: AccountCategory;
+  MMR: MMRCategory;
 }
