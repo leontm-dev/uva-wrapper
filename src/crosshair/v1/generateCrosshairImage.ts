38dde01d0249593547d58c31c147bc4e9b46c8d0
Leon <LAnneken09@gmail.com>
2026-07-13 21:55:05 +0200

    Added the generateCrosshairImage endpoint to v1

---
+++ src/crosshair/v1/generateCrosshairImage.ts
@@ -0,0 +1,10 @@
+import { doApiRequest } from "../../lib/request-handler";
+
+export async function v1_generateCrosshairImage(apiKey: string, id?: string) {
+  return await doApiRequest(
+    apiKey,
+    "/v1/crosshair/generate",
+    { id },
+    { method: "GET" },
+  );
+}
--- src/crosshair/v1/index.ts
+++ src/crosshair/v1/index.ts
@@ -1,3 +1,14 @@
+import { v1_generateCrosshairImage } from "./generateCrosshairImage";
+
 export class v1_CrosshairCategory {
   constructor(readonly apiKey: string) {}
+
+  /**
+   * @link https://docs.henrikdev.xyz/api-reference/valorant/generate-crosshair-image-v1
+   * @param id Crosshair code
+   * @returns Crosshair image generated successfully
+   */
+  async generateImage(id?: string) {
+    return await v1_generateCrosshairImage(this.apiKey, id);
+  }
 }
