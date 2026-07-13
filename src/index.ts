46b825e30eccfe8d20f3c29b7c7afb8a980b4ca7
Leon <LAnneken09@gmail.com>
2026-07-13 15:39:21 +0200

    Created and added match details category

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { AccountCategory } from "./account";
+import { MatchDetailsCategory } from "./match-details";
 import { MatchesCategory } from "./matches";
 import { MMRCategory } from "./mmr";
 import { MMRHistoryCategory } from "./mmr-history";
@@ -6,12 +7,14 @@ import { MMRHistoryCategory } from "./mmr-history";
 export class UnofficialValorantAPI {
   constructor(readonly apiKey: string) {
     this.Account = new AccountCategory(this.apiKey);
+    this.Match_Details = new MatchDetailsCategory(this.apiKey);
     this.Matches = new MatchesCategory(this.apiKey);
     this.MMR = new MMRCategory(this.apiKey);
     this.MMR_History = new MMRHistoryCategory(this.apiKey);
   }
 
   Account: AccountCategory;
+  Match_Details: MatchDetailsCategory;
   Matches: MatchesCategory;
   MMR: MMRCategory;
   MMR_History: MMRHistoryCategory;
---
+++ src/match-details/index.ts
@@ -0,0 +1,3 @@
+export class MatchDetailsCategory {
+  constructor(readonly apiKey: string) {}
+}
