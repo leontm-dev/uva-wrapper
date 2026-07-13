2c8d08016e47848f75d68a00a3c2f47fa9340fa5
Leon <LAnneken09@gmail.com>
2026-07-13 13:13:03 +0200

    Added a request handler

---
+++ src/lib/request-handler.ts
@@ -0,0 +1,29 @@
+import { apiURL } from "../constants/api.const";
+import {
+  DefaultApiResponse,
+  DefaultErrorsResponse,
+} from "../types/response.type";
+
+export async function doApiRequest<T>(
+  apiKey: string,
+  url: string,
+  query?: Record<string, string | undefined>,
+  init?: RequestInit & { method: "GET" | "POST" },
+): Promise<DefaultApiResponse<T> | DefaultErrorsResponse> {
+  return await fetch(
+    apiURL + url + Object.entries(query || {}).length
+      ? "?"
+      : "" +
+          Object.entries(query || {})
+            .filter((e) => typeof e[1] === "string")
+            .map((e) => e[0] + "=" + e[1]!.toString())
+            .join("&"),
+    {
+      ...init,
+      headers: {
+        ...init?.headers,
+        Authorization: apiKey,
+      },
+    },
+  ).then((res) => res.json());
+}
